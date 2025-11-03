adfc398aea8e8e3a76e4786ef2414fa0b5cefd56
Patrick (Patrick) Pajda <pajp3780@362charon>
2025-11-02 17:19:53 -0700

    added extra test cases

--- build/main.d
+++
@@ -1,2 +0,0 @@
-build/main.o: src/main.cc include/include.h
-include/include.h:
--- build/test_header_driver_t.d
+++ build/test_header_driver_t.d
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-build/test_header_driver_t.o: test/header_driver_t.cc \
- test/header_driver_t.h
-=======
 build/test_header_driver_t.o: test/header_driver_t.cc include/include.h \
-  test/header_driver_t.h
-
+ test/header_driver_t.h
 include/include.h:
-
->>>>>>> 7386aa8e227384421aca3b361128bf979111c7d7
 test/header_driver_t.h:
